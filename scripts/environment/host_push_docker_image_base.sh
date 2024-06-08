a9d63a5900e54f5dec69df41c25b7dcb73c0583c
Eamon Mahon <eamon.mahon@ucd.ie>
2024-06-08 15:05:35 +0100

    initial modifications to run on whitebox
    changes made from mac

--- scripts/environment/host_push_docker_image_base.sh
+++ scripts/environment/host_push_docker_image_base.sh
@@ -4,6 +4,11 @@ WORK_DIR=$(pwd)
 # Import global variables
 source $WORK_DIR/scripts/config/env.sh
 
+echo "testing python version"
+python --version
+echo "testing python3 version"
+python3 --version
+
 # Copy the base docker to the server
 echo 'Copy docker image for base to servers'
 PYTHONPATH=$PYTHONPATH:$WORK_DIR python scripts/environment/host_push_docker_image_base.py $WORK_DIR/scripts/config/servers.txt $WORK_DIR/tmp/$DOCKER_IMAGE_BASE_FILENAME
