13db0764c315c122ae737998a6b511b2c92cf493
eamon-whitebox <s2602050@ed.ac.uk>
2024-06-17 19:33:24 +0000

    testing old method of loading resnet152

--- scripts/experiment.sh
+++ scripts/experiment.sh
@@ -30,31 +30,31 @@ echo "host_build_docker_image_ready_model.sh done"
 # ####################
 # ### Plot figures ###
 
-mkdir output
+# mkdir output
 
-echo "Warm up servers"
-# Warm up servers
-bash scripts/environment/host_run_warmup.sh
+# echo "Warm up servers"
+# # Warm up servers
+# bash scripts/environment/host_run_warmup.sh
 
-echo "Plot figure 5"
-# PLot figure 5
-bash scripts/figures/figure5/plot_figure.sh
+# echo "Plot figure 5"
+# # PLot figure 5
+# bash scripts/figures/figure5/plot_figure.sh
 
-echo "Plot figure 6"
-# PLot figure 6
-bash scripts/figures/figure6/plot_figure.sh
+# echo "Plot figure 6"
+# # PLot figure 6
+# bash scripts/figures/figure6/plot_figure.sh
 
-echo "Plot figure 7"
-# PLot figure 7
-bash scripts/figures/figure7/plot_figure.sh
+# echo "Plot figure 7"
+# # PLot figure 7
+# bash scripts/figures/figure7/plot_figure.sh
 
-echo "Plot figure 8"
-# PLot figure 8
-bash scripts/figures/figure8/plot_figure.sh
+# echo "Plot figure 8"
+# # PLot figure 8
+# bash scripts/figures/figure8/plot_figure.sh
 
-echo "Plot figure 9"
-# PLot figure 9
-bash scripts/figures/figure9/plot_figure.sh
+# echo "Plot figure 9"
+# # PLot figure 9
+# bash scripts/figures/figure9/plot_figure.sh
 
 # ### Plot figures END ###
 # ########################
