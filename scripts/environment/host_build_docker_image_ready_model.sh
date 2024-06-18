dcd510d5d688e5713ecb3b219bb78daccc10b8f0
eamon-whitebox <s2602050@ed.ac.uk>
2024-06-18 16:03:33 +0000

    trying with latest version of vision

--- scripts/environment/host_build_docker_image_ready_model.sh
+++ scripts/environment/host_build_docker_image_ready_model.sh
@@ -7,7 +7,6 @@ source $WORK_DIR/scripts/config/env.sh
 # Build the pipeswitch docker on the server
 echo 'Build docker image for ready model on servers'
 echo "------------------------- [EAMON][LOG] HOST BUILD DOCKER IMAGE READY MODEL FROM SHELL SCRIPT"
-echo "------------------------- [EAMON][LOG] Current working directory: $PWD"
 echo 
 
 PYTHONPATH=$PYTHONPATH:$WORK_DIR python scripts/environment/host_build_docker_image_ready_model.py $WORK_DIR/scripts/config/servers.txt
